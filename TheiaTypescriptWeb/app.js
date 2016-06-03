3a424806c2dba96c310dd70615e49d34795d283f
kushal <kushaldeveloper@gmail.com>
2016-06-03 13:38:05 -0400

    remove part to change text
    can't figure out how to change text so this will have to do for now

--- TheiaTypescriptWeb/app.js
+++ TheiaTypescriptWeb/app.js
@@ -1,22 +1,19 @@
 var Greeter = (function () {
-    function Greeter(element, stopButton) {
+    function Greeter(element) {
         this.isRunning = false;
         this.element = element;
         this.element.innerHTML += "The UTC time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
-        this.stopButton.innerHTML = "Stop";
     }
     Greeter.prototype.start = function () {
         var _this = this;
         this.isRunning = true;
-        this.stopButton.innerHTML = "Stop";
         this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toLocaleString(); }, 100);
     };
     Greeter.prototype.stop = function () {
         this.isRunning = false;
-        this.stopButton.innerHTML = "Start";
         clearTimeout(this.timerToken);
     };
     return Greeter;
@@ -24,7 +21,7 @@ var Greeter = (function () {
 window.onload = function () {
     var el = document.getElementById('content');
     var btn1 = document.getElementsByName("btnStop")[0];
-    var greeter = new Greeter(el, btn1);
+    var greeter = new Greeter(el);
     greeter.start();
     var myEl = document.getElementById('btnStop');
     myEl.addEventListener('click', function () {
--- TheiaTypescriptWeb/app.ts
+++ TheiaTypescriptWeb/app.ts
@@ -3,27 +3,23 @@
     span: HTMLElement;
     timerToken: number;
     isRunning: boolean;
-    stopButton: HTMLButtonElement;
 
-    constructor(element: HTMLElement, stopButton: HTMLButtonElement) {
+    constructor(element: HTMLElement) {
         this.isRunning = false;
         this.element = element;
         this.element.innerHTML += "The UTC time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
-        this.stopButton.innerHTML = "Stop";
     }
 
     start() {
         this.isRunning = true;
-        this.stopButton.innerHTML = "Stop";
         this.timerToken = setInterval(() => this.span.innerHTML = new Date().toLocaleString(), 100);
     }
 
     stop() {
         this.isRunning = false;
-        this.stopButton.innerHTML = "Start";
         clearTimeout(this.timerToken);
     }
 
@@ -32,7 +28,7 @@
 window.onload = () => {
     var el = document.getElementById('content');
     var btn1 = (<HTMLButtonElement[]><any>document.getElementsByName("btnStop"))[0];
-    var greeter = new Greeter(el, btn1);
+    var greeter = new Greeter(el);
     greeter.start();
 
     var myEl = document.getElementById('btnStop');
