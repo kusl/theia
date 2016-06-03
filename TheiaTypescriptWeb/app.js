ef79014532e8569abbbdf5db4f4e737bb93a74e1
kushal <kushaldeveloper@gmail.com>
2016-06-03 12:42:42 -0400

    use button element
    https://stackoverflow.com/questions/12686927/typescript-casting-htmlelement

--- TheiaTypescriptWeb/app.js
+++ TheiaTypescriptWeb/app.js
@@ -1,23 +1,39 @@
 var Greeter = (function () {
-    function Greeter(element) {
+    function Greeter(element, stopButton) {
+        this.isRunning = false;
         this.element = element;
-        this.element.innerHTML += "The time is: ";
+        this.element.innerHTML += "The UTC time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
+        this.stopButton.value = "Stop";
     }
     Greeter.prototype.start = function () {
         var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
+        this.isRunning = true;
+        this.stopButton.value = "Stop";
+        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toLocaleString(); }, 100);
     };
     Greeter.prototype.stop = function () {
+        this.isRunning = false;
+        this.stopButton.value = "Start";
         clearTimeout(this.timerToken);
     };
     return Greeter;
 }());
 window.onload = function () {
     var el = document.getElementById('content');
-    var greeter = new Greeter(el);
+    var btn1 = document.getElementsByName("btnStop")[0];
+    var greeter = new Greeter(el, btn1);
     greeter.start();
+    var myEl = document.getElementById('btnStop');
+    myEl.addEventListener('click', function () {
+        if (greeter.isRunning) {
+            greeter.stop();
+        }
+        else {
+            greeter.start();
+        }
+    }, false);
 };
 //# sourceMappingURL=app.js.map
--- TheiaTypescriptWeb/app.ts
+++ TheiaTypescriptWeb/app.ts
@@ -2,20 +2,28 @@
     element: HTMLElement;
     span: HTMLElement;
     timerToken: number;
+    isRunning: boolean;
+    stopButton: HTMLButtonElement;
 
-    constructor(element: HTMLElement) {
+    constructor(element: HTMLElement, stopButton: HTMLButtonElement) {
+        this.isRunning = false;
         this.element = element;
-        this.element.innerHTML += "The time is: ";
+        this.element.innerHTML += "The UTC time is: ";
         this.span = document.createElement('span');
         this.element.appendChild(this.span);
         this.span.innerText = new Date().toUTCString();
+        this.stopButton.value = "Stop";
     }
 
     start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+        this.isRunning = true;
+        this.stopButton.value = "Stop";
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toLocaleString(), 100);
     }
 
     stop() {
+        this.isRunning = false;
+        this.stopButton.value = "Start";
         clearTimeout(this.timerToken);
     }
 
@@ -23,6 +31,18 @@
 
 window.onload = () => {
     var el = document.getElementById('content');
-    var greeter = new Greeter(el);
+    var btn1 = (<HTMLButtonElement[]><any>document.getElementsByName("btnStop"))[0];
+    var greeter = new Greeter(el, btn1);
     greeter.start();
+
+    var myEl = document.getElementById('btnStop');
+
+    myEl.addEventListener('click', function () {
+        if (greeter.isRunning) {
+            greeter.stop();
+        }
+        else {
+            greeter.start();
+        }
+    }, false);
 };
