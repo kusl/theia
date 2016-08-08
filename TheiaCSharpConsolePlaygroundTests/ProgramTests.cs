54fbc804918fa1e52e7ed12c539573e02a0d5543
kushal <kushaldeveloper@gmail.com>
2016-08-08 19:04:36 -0400

    add tests

---
+++ TheiaCSharpConsolePlaygroundTests/ProgramTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheiaCSharpConsolePlaygroundTests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainReturns()
+        {
+            String[] args = { "apple", "ball" };
+            TheiaCSharpConsolePlayground.Program.Main(args);
+        }
+    }
+}
